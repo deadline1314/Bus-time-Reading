e051420538b25df9500afb61bb0fcdafcb5b85de
Ruotian(Kerwin) Wang <deadline1314@gmail.com>
2016-10-09 14:41:06 -0700

    updates

--- Algriothms/src/DynamicProgramming/GetMinCoins.java
+++ Algriothms/src/DynamicProgramming/GetMinCoins.java
@@ -7,20 +7,13 @@ public class GetMinCoins {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[0].length; j++) {
 				if (coins[i] > j) {
-					if (i > 0)
-						matrix[i][j] = matrix[i - 1][j];
-					else
-						matrix[i][j] = 0;
+					matrix[i][j] = matrix[i - 1][j];
 				} else {
-					matrix[i][j] = matrix[i][j - coins[i]] + 1;
+					matrix[i][j] = Math.min(matrix[i-1][j] , matrix[i][j - coins[i]] + 1);
 				}
 			}
 		}
-		int result = Integer.MAX_VALUE;
-		for (int i = 0; i < matrix.length; i++) {
-			result = result > matrix[i][total] ? matrix[i][total] : result;
-		}
-		return result;
+		return matrix[matrix.length-1][matrix[0].length-1];
 	}
 
 	public static void main(String[] args) {
